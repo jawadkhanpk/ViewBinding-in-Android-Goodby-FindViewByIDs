c7ae772ba4262907e65cfffc5f60173b3351482b
jawadkhanpk <jawadkhan.tech@gmail.com>
2022-09-20 12:05:36 +0500

    Final Implementation

--- app/src/main/java/com/example/viewbindinginandroid/MainActivity.java
+++ app/src/main/java/com/example/viewbindinginandroid/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.viewbindinginandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.viewbindinginandroid.databinding.ActivityMainBinding;
 
@@ -15,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(activityMainBinding.getRoot());
 
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        activityMainBinding.t1.setText("Hello this is the feature of ViewBinding");
+        activityMainBinding.btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button Working", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
